7b2086532d4f874bb844cf53610996e583b09949
Dilshan Sankalpa <2019cs040@stu.ucsc.lk>
2025-01-13 15:27:55 +0530

    feat: :sparkles: first comment

---
+++ src/components/Footer.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import MailIcon from '@mui/icons-material/Mail';
+import '../assets/styles/Footer.scss'
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+            <a href="https://github.com/dilshanSankalpa" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/dilshan-sankalpa-dissanayake/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="mailto:dilshansankalpa98@gmail.com" target="_blank" rel="noreferrer"><MailIcon/></a>
+      </div>
+      {/* <p>A portfolio designed & built by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a> with 💜</p> */}
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Main.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import MailIcon from '@mui/icons-material/Mail';
+import '../assets/styles/Main.scss';
+
+function Main() {
+
+  return (
+    <div className="container">
+      <div className="about-section">
+        <div className="image-wrapper">
+          <img src="/src/assets/images/profil_avatar.jpg" alt="Avatar" />
+        </div>
+        <div className="content">
+          <div className="social_icons">
+            <a href="https://github.com/dilshanSankalpa" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/dilshan-sankalpa-dissanayake/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="mailto:dilshansankalpa98@gmail.com" target="_blank" rel="noreferrer"><MailIcon/></a>
+          </div>
+          <h1>Dilshan Sankalpa</h1>
+          <p>Software Engineer | ML Engineer | DevOps Engineer | Data scientist</p>
+
+          <div className="mobile_social_icons">
+            <a href="https://github.com/dilshanSankalpa" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/dilshan-sankalpa-dissanayake/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="mailto:dilshansankalpa98@gmail.com" target="_blank" rel="noreferrer"><MailIcon/></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
