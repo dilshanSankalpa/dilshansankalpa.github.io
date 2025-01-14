b57833b8726e658ae960f5b5c4a0ade316287947
Dilshan Sankalpa <2019cs040@stu.ucsc.lk>
2025-01-15 03:01:01 +0530

    chore: update education section with new degree and institution details

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -20,8 +20,8 @@ function Education() {
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faGraduationCap} />}
           >
-            <h3 className="vertical-timeline-element-title">Technology Consultant</h3>
-            <h4 className="vertical-timeline-element-subtitle">Dallas, TX</h4>
+            <h3 className="vertical-timeline-element-title">Master's in Data analytics</h3>
+            <h4 className="vertical-timeline-element-subtitle">University of Kelaniya</h4>
             <p>
               Full-stack Web Development, GenAI/LLM, Project Management, Business Development
             </p>
